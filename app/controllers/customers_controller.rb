82842742515d8537a24dc3733e0818c6e566f06b
rayprastya <54200114+rayprastya@users.noreply.github.com>
2022-04-23 02:47:57 +0000

    feat: blocking user that register without registering their email

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -24,12 +24,14 @@ class CustomersController < ApplicationController
     @customer = Customer.new(customer_params)
 
     respond_to do |format|
-      if @customer.save
-        format.html { redirect_to customer_url(@customer), notice: "Customer was successfully created." }
+      if @customer.save and @customer['email'].include? "@"
+        format.html { redirect_to customer_url(@customer), notice: "Customer was successfully created."}
         format.json { render :show, status: :created, location: @customer }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @customer.errors, status: :unprocessable_entity }
+
+        @customer.destroy
       end
     end
   end
